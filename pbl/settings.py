6c30156b1eecdf5cc55f59cff562126ed53aad38
sanchitkripalani47 <67781787+sanchitkripalani47@users.noreply.github.com>
2021-06-08 23:33:16 +0530

    Update settings.py

--- pbl/settings.py
+++ pbl/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'FirstDataBase',
         'USER': 'postgres',
-        'PASSWORD': 'collatioisthebest',
+        'PASSWORD': '***********',
         'HOST': 'localhost',
         'PORT': '5432' 
     }
